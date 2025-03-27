6da83b225b0f58a823dfb768d8f175fd1d5fe42a
SP3cTRum-GH <min1324354@gmail.com>
2025-03-27 11:24:42 +0900

    update Homework0326

--- src/Homework/Homework0326_1.java
+++ src/Homework/Homework0326_1.java
@@ -9,24 +9,48 @@ public class Homework0326_1 {
         final double LIGHT_SPEED = 3e5;
         int day=0;
         double distance = 0;
+        String inputContinue;
         char isContinue = 0;
         Scanner s = new Scanner(System.in);
+        boolean flag = false;
+        String inputDay;
 
         for(;;) {
-            System.out.println("몇일간 가는 거리를 계산하시겠습니까?: ");
-            day = Integer.parseInt(s.nextLine());
+            while (!flag) {
+                System.out.print("몇일간 가는 거리를 계산하시겠습니까?: ");
+                inputDay = s.nextLine();
+                try {
+                    day = Integer.parseInt(inputDay);
+                    if(day < 0) {
+                        System.out.println("양수를 입력해주세요");
+                    }else{
+                        flag = true;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("유효한 날짜를 입력해주세요");
+                }
 
+            }
+            flag = false;
             distance = LIGHT_SPEED * 60 * 60 * 24 * day;
             System.out.println(distance + "km");
 
             do {
                 System.out.print("계속하시겠습니까? (y/n)");
-                isContinue = s.nextLine().toLowerCase().charAt(0);
-
-                if(isContinue != 'n' && isContinue != 'y'){
-                    System.out.println("유효하지않은 입력입니다 다시 입력해주세요");
+                inputContinue = s.nextLine();
+                if(inputContinue.length() ==1) {
+                    isContinue = inputContinue.charAt(0);
+                }else {
+                    System.out.println("한글자만 입력해주세요");
+                    continue;
                 }
-            } while (isContinue != 'n' && isContinue != 'y');
+
+                if(isContinue == 'n' || isContinue == 'y'){
+                    break;
+                }
+                System.out.println("유효하지않은 입력입니다 다시 입력해주세요");
+
+            } while (true);
 
             if (isContinue == 'n') {
                 s.close();
